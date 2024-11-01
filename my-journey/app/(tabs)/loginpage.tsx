e5ef3ff0c948523223c5a4a8fb34e1269a3bac59
89Omer <mohammad.omer.raza@gmail.com>
2024-11-01 09:48:46 +0000

    fixing route issue

--- my-journey/app/(tabs)/loginpage.tsx
+++ my-journey/app/(tabs)/loginpage.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "../../screens/Login";
+import LoginScreen from "@/screens/Login";
 
 export default function loginpage() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <LoginScreen />
+
     </View>
   );
 }
